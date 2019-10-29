fb798d02cc55cb0e66667500e8438e5db3623324
root <root@localhost.localdomain>
2019-10-29 13:18:09 +0600

    done

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import(
-	//"fmt"
+	"encoding/json"
+	"fmt"
 	"github.com/streadway/amqp"
 	"log"
+	"os"
 	//"math/rand"
 	//"time"
 )
@@ -29,5 +31,45 @@ func main() {
 	handleError(err, "Could not declare `add` queue")
 	err = amqpChannel.Qos(1, 0, false)
 	handleError(err, "Could not configure QoS")
-	
+	fmt.Printf("%s",queue.Name)
+	messageChannel, err := amqpChannel.Consume(
+		queue.Name,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	handleError(err, "Could not register consumer")
+	stopChan := make(chan bool)
+
+	go func() {
+		log.Printf("Consumer ready, PID: %d", os.Getpid())
+		for d := range messageChannel {
+			log.Printf("Received a message: %s", d.Body)
+
+			addTask := &AddTask{}
+
+			err := json.Unmarshal(d.Body, addTask)
+
+			if err != nil {
+				log.Printf("Error decoding JSON: %s", err)
+			}
+
+			log.Printf("Result of %d + %d is : %d", addTask.Number1, addTask.Number2, addTask.Number1+addTask.Number2)
+
+			if err := d.Ack(false); err != nil {
+				log.Printf("Error acknowledging message : %s", err)
+			} else {
+				log.Printf("Acknowledged message")
+			}
+
+		}
+	}()
+
+	// Stop for program termination
+	<-stopChan
+
 }
+
